59d9d417caf4323ce9bc2a107cc37cc99dd4f140
Woodcastle <56039310+Woodcastle@users.noreply.github.com>
2020-03-12 16:36:30 -0400

    Add files via upload
    The frame of the program, the fucntions are not fleshed out but the requirements are posted to their java doc area

---
+++ CommandDesignPattern/src/csce247/assignments/command/LoadCommand.java
@@ -0,0 +1,16 @@
+package csce247.assignments.command;
+
+public class LoadCommand implements Command{
+	
+	
+	public LoadCommand(Document doc) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void execute() {
+		
+	}
+}
---
+++ CommandDesignPattern/src/csce247/assignments/command/PrintCommand.java
@@ -0,0 +1,17 @@
+package csce247.assignments.command;
+
+public class PrintCommand implements Command{
+	
+	public PrintCommand(Document doc) {
+		
+	}
+	
+	/**
+	 * 
+	 * 
+	 */
+	public void execute() {
+		
+	}
+
+}
