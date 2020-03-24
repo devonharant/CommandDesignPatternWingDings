f5977e3dea6eabaac84abb285b018b8c981257d7
canixon <christopherdahl.nixon@gmail.com>
2020-03-23 22:38:15 -0400

    working on input handler

--- CommandDesignPattern/src/csce247/assignments/command/CommandDesignPattern/src/csce247/assignments/command/InputHandler.java
+++ CommandDesignPattern/src/csce247/assignments/command/CommandDesignPattern/src/csce247/assignments/command/InputHandler.java
@@ -1,38 +1,37 @@
 package csce247.assignments.command;
 
-import java.util.*;
+import java.util.HashMap;
 
 /**
- * *INSERT CLASS NAME AND DESC HERE*
+ * Input Handler Class This class takes the input and decides what the system will do after
+ *
  * @author WingDings
  *
  */
 public class InputHandler {
-	
-	/**
-	 * Holds a hashmap for the commands, and their input keys
-	It is initialized in the constructor
-	key -> value
-	"load"-> LoadCommand
-	"save" -> SaveCommand
-	"spell" -> SpellCheckCommand
-	"print" -> PrintCommand
-	 */
-	private HashMap<String, Command> commands;
-	
-	/**
-	 * 
-	 * @input
-	 */
-	public InputHandler(Document document) {
-		//Todo 
-	}
-	
-	/**
-	 * intputEntered method that will call the appropriate command that's key matches the data parameter.
-	 * @param data
-	 */
-	public void inputEntered(String data) {
-		//Todo
-	}
+
+  /**
+   * Holds a hashmap for the commands, and their input keys It is initialized in the constructor key
+   * -> value "load"-> LoadCommand "save" -> SaveCommand "spell" -> SpellCheckCommand "print" ->
+   * PrintCommand
+   */
+  private HashMap<String, Command> commands;
+
+  /**
+   *
+   * @input
+   */
+  public InputHandler(Document document) {
+    commands = new HashMap<String, Command>();
+  }
+
+  /**
+   * intputEntered method that will call the appropriate command that's key matches the data
+   * parameter.
+   *
+   * @param data
+   */
+  public void inputEntered(String data) {
+    //commands<data>.execute();
+  }
 }
