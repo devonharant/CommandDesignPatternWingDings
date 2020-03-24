dfcc2be10ec1a7bfb79f1015f40026a0c71e4774
canixon <christopherdahl.nixon@gmail.com>
2020-03-23 20:36:09 -0400

    completed Document.java

--- CommandDesignPattern/src/csce247/assignments/command/CommandDesignPattern/src/csce247/assignments/command/Document.java
+++ CommandDesignPattern/src/csce247/assignments/command/CommandDesignPattern/src/csce247/assignments/command/Document.java
@@ -2,51 +2,58 @@ package csce247.assignments.command;
 
 /**
  * *INSERT CLASS NAME AND DESC HERE*
+ *
  * @author WingDings
  *
  */
 public class Document {
-	
-	public String name;
-	
-	/**
-	 * Documented named ... is created
-	 * @param name
-	 */
-	public Document(String name) {
-		this.name = name;
-		System.out.println("Document named " + name + " is created");
-	}
-	
-	/**
-	 * load method that simulates loading
-	 * @return document ... is loaded into view
-	 */
-	public void load() {
-		
-	}
-	
-	/**
-	 * spell method that simulates spelling 
-	 * @return document ... is being checked for spelling errors
-	 */
-	public void spell() {
-		
-	}
-	
-	/**
-	 * save method that simulates saving
-	 * @return document ... is being saved...
-	 */
-	public void save() {
-		
-	}
-	
-	/**
-	 * print method that simulates printing
-	 * @return document ... is printing...
-	 */
-	public void print() {
-		
-	}
+
+  public String name;
+
+  /**
+   * Documented named ... is created
+   *
+   * @param name
+   */
+
+  public Document(String name) {
+    this.name = name;
+    System.out.println("Document named " + name + " is created");
+  }
+
+  /**
+   * load method that simulates loading
+   *
+   * @return document ... is loaded into view
+   */
+  public void load() {
+    System.out.println("Document " + name + " is loaded into view");
+  }
+
+  /**
+   * spell method that simulates spelling
+   *
+   * @return document ... is being checked for spelling errors
+   */
+  public void spell() {
+    System.out.println("Document " + name + " is being checked for spelling errors");
+  }
+
+  /**
+   * save method that simulates saving
+   *
+   * @return document ... is being saved...
+   */
+  public void save() {
+    System.out.println("Document " + name + " is being saved");
+  }
+
+  /**
+   * print method that simulates printing
+   *
+   * @return document ... is printing...
+   */
+  public void print() {
+    System.out.println("Document " + name + " is printing");
+  }
 }
